fad5c36918f1e3bdb13dcef779fb334dfb93171e
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-01-04 15:33:05 +0530

    add

---
+++ Module-3/Array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int array[]={56,56,78,98,45};
+    int i;
+    
+    for ( i = 0; i < 5; i++)
+    {
+        printf("\nArray[%d]:%d",i,array[i]);
+    }
+    
+    
+}
--- Module-3/Nested-Loop.c
+++ Module-3/Nested-Loop.c
@@ -5,9 +5,9 @@ void main()
     int count = 5;
     for ( i = 0; i < count; i++)
     {
-        for ( j = 5; j > i; j--)
+        for ( j = 0; j <= i; j++)
         {
-            printf(" %d",j);
+            printf(" %d",i);
         }
         printf("\n");
     }
